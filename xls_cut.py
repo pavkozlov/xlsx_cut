1b304c7956fe94422369454c8a5ae8a0759b0a39
Pavel Kozlov <it.pavelkozlov@gmail.com>
2019-08-17 19:41:02 +0300

    improved work speed (change dict to tuple), created context manager

--- xls_cut.py
+++ xls_cut.py
@@ -1,58 +1,63 @@
 import xlrd
 import xlwt
 import os
+from contextlib import contextmanager
+from collections import namedtuple
+
+FirstSecond = namedtuple('FirstSecond', 'first second')
 
 
 class XlsChanger:
     def __init__(self):
-        self.max_length = int(input('Максимальная длинна строки: '))
+        self.max_length = int(input('Максимальная строка: '))
         self.file = self.select_file()
         self.book = xlrd.open_workbook(self.file)
         self.sheet = self.select_sheet()
-        self.column = int(input('Выберите колонку (А = 1, B = 2, C =3, D = 4, E = 5 и т.д.): ')) - 1
-        self.write(self.save_to_csv())
+        self.column = int(input('Выберите колонку (А = 1, B = 2 и т.д.): ')) - 1
+        self.save_to_file()
 
-    def save_to_csv(self):
-        result = list()
-        for i in range(self.sheet.nrows):
-            line = self.sheet.row(i)[self.column].value
-            res = self.format_line(line) if len(line) > 30 else {'f': line, 's': ''}
-            result.append(res)
-        return result
+    def save_to_file(self):
+        result = (self.check_line(self.sheet.row(i)[self.column].value) for i in range(self.sheet.nrows))
+        with xlwt_context(self.file) as wb:
+            ws = wb.add_sheet(self.sheet.name)
+            for num, (f, s) in enumerate(result):
+                ws.write(num, self.column, f)
+                ws.write(num, self.column + 1, s)
 
-    def select_file(self):
-        print(f'Выберите нужный файл из {len(os.listdir())}')
-        for file in enumerate(os.listdir()):
-            print(f'{file[0] + 1}) {file[1]}')
+    def check_line(self, line: str) -> namedtuple:
+        res = self.format_line(line) if len(line) > 30 else FirstSecond(line, None)
+        return res
+
+    def select_file(self) -> str:
+        print(f'Выберите файл:')
+        for num, file in enumerate(os.listdir(), start=1):
+            print(num, file)
         file_number = int(input('Ответ: ')) - 1
-        file = os.listdir()[file_number]
-        print(f'Выбран файл: {file}')
-        return file
+        return os.listdir()[file_number]
 
-    def select_sheet(self):
-        print(f'Выберите нужный лист из {self.book.nsheets}: ')
-        for sheet in enumerate(self.book.sheet_names()):
-            print(f'{sheet[0] + 1}) {sheet[1]}')
+    def select_sheet(self) -> xlrd.sheet.Sheet:
+        print(f'Выберите лист: ')
+        for num, sheet in enumerate(self.book.sheet_names(), start=1):
+            print(num, sheet)
         sheet_number = int(input('Ответ: ')) - 1
-        sheet = self.book.sheet_by_index(sheet_number)
-        print(f'Выбран лист: {sheet.name} ({sheet.nrows} строк * {sheet.ncols} колонок)')
-        return sheet
+        return self.book.sheet_by_index(sheet_number)
 
-    def write(self, result):
-        wb = xlwt.Workbook()
-        ws = wb.add_sheet(self.sheet.name)
-        for row in enumerate(result):
-            ws.write(row[0], self.column, row[1]['f'])
-            ws.write(row[0], self.column + 1, row[1]['s'])
-        wb.save(f'MODIFED_{self.file}')
-
-    def format_line(self, line):
+    def format_line(self, line: str) -> namedtuple:
         counter = self.max_length
         my_line = line[:counter]
         while my_line[-1] != ' ':
             counter -= 1
             my_line = line[:counter]
-        return {'f': my_line, 's': line[counter:]}
+        return FirstSecond(my_line, line[counter:])
+
+
+@contextmanager
+def xlwt_context(file):
+    try:
+        wb = xlwt.Workbook()
+        yield wb
+    finally:
+        wb.save(f'MODIFED_{file}')
 
 
 if __name__ == '__main__':
